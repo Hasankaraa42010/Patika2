2f0785af24f3d912aa9fa9ae4037df03be76adf5
hasan hüseyin karacığay <77877177+Hasankaraa42010@users.noreply.github.com>
2022-03-08 19:33:52 +0300

    Add files via upload

---
+++ src/dongu/Mukemmel.java
@@ -0,0 +1,26 @@
+package dongu;
+
+import java.util.Scanner;
+
+public class Mukemmel {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int toplam = 0;
+
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = input.nextInt();
+
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam = toplam + i;
+            }
+        }
+        if (sayi == toplam) {
+            System.out.println(sayi + " Mükemmel sayidir.");
+        } else {
+            System.out.println(sayi + " Mükemmel sayi degildir.");
+        }
+
+    }
+}
