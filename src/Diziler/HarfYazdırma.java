5921a319dfcae295e0fe6db8a407b4ee5a002d36
hasan hüseyin karacığay <77877177+Hasankaraa42010@users.noreply.github.com>
2022-03-14 11:57:56 +0300

    Add files via upload

---
+++ src/Diziler/HarfYazdırma.java
@@ -0,0 +1,28 @@
+package Diziler;
+
+public class HarfYazdırma {
+    public static void main(String[] args) {
+        String dizi[][]=new String[7][4];
+        for (int i = 0; i < dizi.length ; i++) {
+            for (int j = 0; j <dizi[i].length ; j++) {
+                if (i==0 || i==3 || i==6){
+                    dizi[i][j]=" * ";
+                }
+                else if (j==0 || j==3){
+                    dizi[i][j]=" * ";
+                }
+                else {
+                    dizi[i][j]="   ";
+                }
+            //0 3 6
+            }
+        }
+        for (String[] row:dizi){
+            for (String col:row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
